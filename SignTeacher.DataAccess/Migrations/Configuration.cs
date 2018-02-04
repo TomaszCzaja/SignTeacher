1a66243cfd7ebd800cb613d96aa5934a1fd94328
ToomekCz <tomek1525@outlook.com>
2018-02-04 14:01:20 +0100

    ST - Create data access layer with entity framework

---
+++ SignTeacher.DataAccess/Migrations/Configuration.cs
@@ -0,0 +1,24 @@
+using System.Data.Entity.Migrations;
+using SignTeacher.Model;
+
+namespace SignTeacher.DataAccess.Migrations
+{
+    internal sealed class Configuration : DbMigrationsConfiguration<SignTeacherDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(SignTeacherDbContext context)
+        {
+            context.Users.AddOrUpdate(
+                f => f.FirstName,
+                new User() { FirstName = "Thomas", LastName = "Huber" },
+                new User() { FirstName = "Andreas", LastName = "Boehler" },
+                new User() { FirstName = "Julia", LastName = "Huber" },
+                new User() { FirstName = "Chrissi", LastName = "Egin" }
+            );
+        }
+    }
+}
---
+++ SignTeacher.DataAccess/SignTeacherDbContext.cs
@@ -0,0 +1,21 @@
+﻿using SignTeacher.Model;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace SignTeacher.DataAccess
+{
+    public class SignTeacherDbContext : DbContext
+    {
+        public SignTeacherDbContext() : base("SignTeacherDb")
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
