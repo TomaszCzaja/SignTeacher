6b3a39158a74832e57e1fe7ba891b04294c4b054
ToomekCz <tomek1525@outlook.com>
2018-02-10 12:08:25 +0100

    ST - SettingUp LeapMotion

--- SignTeacher.UI/MainWindow.xaml.cs
+++ SignTeacher.UI/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows;
+﻿using System.Linq;
+using System.Windows;
+using Leap;
 using SignTeacher.UI.ViewModel;
 
 namespace SignTeacher.UI
@@ -10,16 +12,28 @@ namespace SignTeacher.UI
         public MainWindow(MainViewModel viewModel)
         {
             InitializeComponent();
+            Controller controller = new Controller();
 
             _viewModel = viewModel;
             DataContext = _viewModel;
 
             Loaded += MainWindow_Loaded;
+            controller.FrameReady += newFrameHandler;
         }
 
         private async void MainWindow_Loaded(object sender, RoutedEventArgs routedEventArgs)
         {
             await _viewModel.LoadAsync();
+        }
+
+        void newFrameHandler(object sender, FrameEventArgs eventArgs)
+        {
+            Frame frame = eventArgs.frame;
+            //The following are Label controls added in design view for the form
+            if (frame.Hands.Any(hand => hand.IsLeft))
+            {
+                var foo = 5;
+            }
         }
     }
 }
